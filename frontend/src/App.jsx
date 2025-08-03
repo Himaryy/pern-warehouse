08477d059155cc1619e0500255ed258b9137aaaa
Hafied <darka433@gmail.com>
2025-08-03 18:31:34 +0700

    install vite+react, tailwind, and shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-blue-800 text-xl text-center">
+      App{" "}
+      <div>
+        <Button>Click Me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
